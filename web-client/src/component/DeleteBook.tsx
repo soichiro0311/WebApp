6d2a4a38a9da11c21359c4c3a830760050ca7a73
soichiro <soichiroseike@gmail.com>
2019-01-01 15:49:09 +0900

    削除機能のコミット漏れをコミット

--- web-client/src/component/DeleteBook.tsx
+++ web-client/src/component/DeleteBook.tsx
@@ -1,35 +1,21 @@
 import * as React from 'react';
 import { Book } from '../model/Book';
-import FloatingButton from 'material-ui/FloatingActionButton'
-import ContentRemove from 'material-ui/svg-icons/content/remove';
-import axios from 'axios'
-const server = 'http://web_server:5000/deleteBook';
+import DeleteButton from '@material-ui/icons/DeleteForever';
+import BookStore from '../store/BookStore';
 
 interface DeleteBookInput {
-    targetBook: Book | null
+    targetBook: Book
+    bookStore: BookStore
 }
 
 // 書籍の全件リストを表示するコンポーネント
 class DeleteBook extends React.Component<DeleteBookInput, {}> {
     public render() {
-        console.log(this.props.targetBook);
         if (this.props.targetBook != null) {
-            return <FloatingButton onClick={this.deleteBook}>
-                <ContentRemove />
-            </FloatingButton>
+            return <DeleteButton color="secondary" onClick={() => this.props.bookStore.onDelete(this.props.targetBook)} />
         } else {
             return null;
         }
-    }
-
-    public deleteBook = () => {
-      return axios.post(server,this.props.targetBook)
-        .then((res) => {
-          console.log(res)
-        })
-        .catch((e) => {
-          console.error(e);
-        });
     }
 }
 
